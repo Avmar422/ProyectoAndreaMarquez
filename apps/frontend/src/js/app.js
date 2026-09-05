468c29bbac682170193ab4a2c0e48d5684d89829
Avmar422 <notnoot584@gmail.com>
2026-09-04 22:00:07 -0400

    cambio del fetch

--- apps/frontend/src/js/app.js
+++ apps/frontend/src/js/app.js
@@ -5,7 +5,7 @@ const contenedorCards = document.querySelector("#contenedor-card");
 botonList.addEventListener("click", async () => {
 
     try {
-        const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+        const respuesta = await fetch("http://localhost:3000/api/miembros");
         const listaUsuarios = await respuesta.json();
 
         botonList.style.display = "none";
